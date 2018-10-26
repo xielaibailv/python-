6a4a2d86cdff69207bb70e6c8ae33d7536ac9549
yoyo <youyou.xu@enesource.com>
2018-10-26 18:29:20 +0800

    爬虫-上市企业模板教程

--- 爬虫-中国上市企业模板教程.py
+++ 爬虫-中国上市企业模板教程.py
@@ -1,12 +1,15 @@
-from urllib.request import Request,urlopen
-import urllib.parse
-import csv
-import pandas
-from urllib.parse import urlencode
-from urllib.error import URLError
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from lxml import etree
+import time
+import pymysql
+from sqlalchemy import create_engine
+from urllib.parse import urlencode  # 编码 URL 字符串
+
+start_time = time.time()  # 计算程序运行时间
 
 
-# 网页提取函数
 def get_one_page(i):
     try:
         headers = {
@@ -26,33 +29,71 @@ def get_one_page(i):
         print('爬取失败')
 
 
-# beatutiful soup解析然后提取表格
 def parse_one_page(html):
     soup = BeautifulSoup(html, 'lxml')
     content = soup.select('#myTable04')[0]  # [0]将返回的list改为bs4类型
     tbl = pd.read_html(content.prettify(), header=0)[0]
     # prettify()优化代码,[0]从pd.read_html返回的list中提取出DataFrame
-
     tbl.rename(columns={'序号': 'serial_number', '股票代码': 'stock_code', '股票简称': 'stock_abbre', '公司名称': 'company_name',
                         '省份': 'province', '城市': 'city', '主营业务收入(201712)': 'main_bussiness_income',
                         '净利润(201712)': 'net_profit', '员工人数': 'employees', '上市日期': 'listing_date', '招股书': 'zhaogushu',
                         '公司财报': 'financial_report', '行业分类': 'industry_classification', '产品类型': 'industry_type',
                         '主营业务': 'main_business'}, inplace=True)
 
-    print(tbl)
+    # print(tbl)
+    return tbl
 
 
-# return tbl
-# rename将表格15列的中文名改为英文名，便于存储到mysql及后期进行数据分析
+# rename将中文名改为英文名，便于存储到mysql及后期进行数据分析
 # tbl = pd.DataFrame(tbl,dtype = 'object') #dtype可统一修改列格式为文本
 
-# 主函数
+def generate_mysql():
+    conn = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='******',
+        port=3306,
+        charset='utf8',
+        db='wade')
+    cursor = conn.cursor()
+
+    sql = 'CREATE TABLE IF NOT EXISTS listed_company (serial_number INT(20) NOT NULL,stock_code INT(20) ,stock_abbre VARCHAR(20) ,company_name VARCHAR(20) ,province VARCHAR(20) ,city VARCHAR(20) ,main_bussiness_income VARCHAR(20) ,net_profit VARCHAR(20) ,employees INT(20) ,listing_date DATETIME(0) ,zhaogushu VARCHAR(20) ,financial_report VARCHAR(20) , industry_classification VARCHAR(20) ,industry_type VARCHAR(100) ,main_business VARCHAR(200) ,PRIMARY KEY (serial_number))'
+    # listed_company是要在wade数据库中建立的表，用于存放数据
+
+    cursor.execute(sql)
+    conn.close()
+
+
+def write_to_sql(tbl, db='wade'):
+    engine = create_engine('mysql+pymysql://root:******@localhost:3306/{0}?charset=utf8'.format(db))
+    try:
+        # df = pd.read_csv(df)
+        tbl.to_sql('listed_company2', con=engine, if_exists='append', index=False)
+    # append表示在原有表基础上增加，但该表要有表头
+    except Exception as e:
+        print(e)
+
+
 def main(page):
-    for i in range(1, page):  # page表示提取页数
+    generate_mysql()
+    for i in range(1, page):
         html = get_one_page(i)
-        parse_one_page(html)
+        tbl = parse_one_page(html)
+        write_to_sql(tbl)
 
 
-# 单进程
+# # 单进程
 if __name__ == '__main__':
-    main(178)  # 共提取n页
+    main(178)
+
+    endtime = time.time() - start_time
+    print('程序运行了%.2f秒' % endtime)
+
+# 多进程
+# from multiprocessing import Pool
+# if __name__ == '__main__':
+# 	pool = Pool(4)
+# 	pool.map(main, [i for i in range(1,178)])  #共有178页
+
+# 	endtime = time.time()-start_time
+# 	print('程序运行了%.2f秒' %(time.time()-start_time))
