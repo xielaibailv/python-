f8dacc32aef32353f83e77c92c1ba38d32a391ab
yoyo <youyou.xu@enesource.com>
2020-05-21 17:41:09 +0800

    更新通讯录

--- test.py
+++ test.py
@@ -1,40 +1,82 @@
-def hwl():
-    str = input('请输入一句话：')
+class TongXunLu:
+    mydict = dict()
+    print('''
+	 |---欢迎进入通讯录程序---|
+	 |---1. 查询联系人资料     ---|
+	 |---2. 插入新的联系人     ---|
+	 |---3. 删除已有联系人     ---|
+	 |---4. 退出通讯录程序     ---|
+	 ''')
 
-    list = []
+    def create_user(self):
+        name = input('请输入联系人姓名：')
+        #
+        # if name not in self.mydict:
+        #     phone = input('请输入联系电话：')
+        #     self.mydict[name] = phone
+        # else:
+        #     print('您输入的姓名在通讯录中已存在--->> {}：{}'.format(name, self.mydict[name]))
+        #     update_user = input('是否修改用户资料（YES/NO）:')
+        #     if update_user == 'YES' or update_user == 'yes':
+        #         phone = input('请输入联系电话：')
+        #         self.mydict[name] = phone
+        #         print('已修改成功')
+        try:
+            print('您输入的姓名在通讯录中已存在--->> {}：{}'.format(name, self.mydict[name]))
+            update_user = input('是否修改用户资料（YES/NO）:')
+            if update_user == 'YES' or update_user == 'yes':
+                phone = input('请输入联系电话：')
+                self.mydict[name] = phone
+                print('已修改成功')
+        except KeyError:
+            phone = input('请输入联系电话：')
+            self.mydict[name] = phone
 
-    for i in str:
-        list.append(i)
+    def delete_user(self):
+        name = input('请输入想要删除的联系人姓名：')
+        # if name in self.mydict:
+        #     del self.mydict[name]
+        #     print('已删除')
+        # else:
+        #     print('通讯录查无此人')
+        try:
+            self.mydict.pop(name)
+            print('已删除')
+        except KeyError:
+            print('通讯录查无此人')
 
-    list1 = list[::-1]
 
-    if list == list1:
-        print('是回文联')
-    else:
-        print('不是回文联')
+    def select_user(self):# 一次判断是否存在，一次取出值，效率不高
+        name = input('请输入想要查询的联系人姓名：')
+        # if name in self.mydict:
+        #     print('{} : {}'.format(name, self.mydict[name]))
+        # else:
+        #     print('通讯录查无此人')
+        try:
+            print('{} : {}'.format(name, self.mydict[name]))
+        except KeyError:
+            print('通讯录查无此人')
 
+    def userbook(self):
+        while 1:
+            try:
+                flag = int(input('请输入相关的指令代码：'))
+            except ValueError:
+                print('指令无法识别')
+                break
+            if flag != 4:
+                if flag == 2:
+                    self.create_user()
+                elif flag == 1:
+                    self.select_user()
+                elif flag == 3:
+                    self.delete_user()
+                else:
+                    print('指令错误')
 
-# hwl()
-
-
-def  sad(*args):
-    a = 0
-    for i in args:
-        letter = 0
-        space = 0
-        nums = 0
-        el = 0
-        for x in i:
-            if x.isdigit():
-                nums += 1
-            elif x == ' ':
-                space += 1
-            elif x.isalpha():
-                letter += 1
             else:
-                el += 1
-        a += 1
-        print('第{}个字符串共有：英文字母{}个，数字{}个，空格{}个，其他字符{}个。' .format(a, letter, nums, space, el))
+                print('------感谢使用通讯录------')
+                break
 
 
-sad('qwert12345 7(*^^%', '124335435','03847dhj  hwekd')
+tongxunlu = TongXunLu().userbook()
