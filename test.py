7a40a3dc8aa82337d62e3148f63fa84cf15ad7f2
yoyo <youyou.xu@enesource.com>
2020-05-21 18:10:31 +0800

    更新登录

--- test.py
+++ test.py
@@ -1,82 +1,34 @@
-class TongXunLu:
-    mydict = dict()
-    print('''
-	 |---欢迎进入通讯录程序---|
-	 |---1. 查询联系人资料     ---|
-	 |---2. 插入新的联系人     ---|
-	 |---3. 删除已有联系人     ---|
-	 |---4. 退出通讯录程序     ---|
-	 ''')
-
-    def create_user(self):
-        name = input('请输入联系人姓名：')
-        #
-        # if name not in self.mydict:
-        #     phone = input('请输入联系电话：')
-        #     self.mydict[name] = phone
-        # else:
-        #     print('您输入的姓名在通讯录中已存在--->> {}：{}'.format(name, self.mydict[name]))
-        #     update_user = input('是否修改用户资料（YES/NO）:')
-        #     if update_user == 'YES' or update_user == 'yes':
-        #         phone = input('请输入联系电话：')
-        #         self.mydict[name] = phone
-        #         print('已修改成功')
-        try:
-            print('您输入的姓名在通讯录中已存在--->> {}：{}'.format(name, self.mydict[name]))
-            update_user = input('是否修改用户资料（YES/NO）:')
-            if update_user == 'YES' or update_user == 'yes':
-                phone = input('请输入联系电话：')
-                self.mydict[name] = phone
-                print('已修改成功')
-        except KeyError:
-            phone = input('请输入联系电话：')
-            self.mydict[name] = phone
-
-    def delete_user(self):
-        name = input('请输入想要删除的联系人姓名：')
-        # if name in self.mydict:
-        #     del self.mydict[name]
-        #     print('已删除')
-        # else:
-        #     print('通讯录查无此人')
-        try:
-            self.mydict.pop(name)
-            print('已删除')
-        except KeyError:
-            print('通讯录查无此人')
-
-
-    def select_user(self):# 一次判断是否存在，一次取出值，效率不高
-        name = input('请输入想要查询的联系人姓名：')
-        # if name in self.mydict:
-        #     print('{} : {}'.format(name, self.mydict[name]))
-        # else:
-        #     print('通讯录查无此人')
-        try:
-            print('{} : {}'.format(name, self.mydict[name]))
-        except KeyError:
-            print('通讯录查无此人')
-
-    def userbook(self):
-        while 1:
-            try:
-                flag = int(input('请输入相关的指令代码：'))
-            except ValueError:
-                print('指令无法识别')
-                break
-            if flag != 4:
-                if flag == 2:
-                    self.create_user()
-                elif flag == 1:
-                    self.select_user()
-                elif flag == 3:
-                    self.delete_user()
-                else:
-                    print('指令错误')
-
+def login():
+    print("""
+          |---新建用户：n/N---| 
+          |---登录账号：e/E---|
+          |---退出程序：q/Q---|
+        """)
+    user = {}
+    while True:
+        code = input('请输入指令代码：')
+        if code == 'N' or code == 'n':  # 新建
+            name = input('请输入用户名：')
+            while name in user:
+                name = input('此用户名已经被使用，请重新输入吧：')
             else:
-                print('------感谢使用通讯录------')
-                break
+                pwd = input('请输入密码：')
+                user[name] = pwd
+                print('注册成功，赶紧试试登录吧')
 
+        elif code == 'E' or code == 'e':  # 登录
+            name = input('请输入用户名：')
+            if name in user:
+                pwd = input('请输入密码：')
+                if user[name] == pwd:
+                    print('欢迎进入登录系统！')
+                else:
+                    print('密码错误')
+            else:
+                print('此账号不存在哦')
+        elif code == 'Q' or code == 'q':  # 退出
+            break
+        else:
+            print('你输的什么指令哦，人家不懂呢')
 
-tongxunlu = TongXunLu().userbook()
+login()
